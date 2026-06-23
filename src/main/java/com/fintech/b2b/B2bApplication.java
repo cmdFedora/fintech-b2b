c20c5d3dcdbcf609cb0565cdd270f24decfbfee3
cmdFedora <dan.gonza.95@hotmail.com>
2026-06-23 10:26:59 -0500

    se actualizo la clase principal del proyecto, agregandole Scheduling para el motor en segundo plano

--- src/main/java/com/fintech/b2b/B2bApplication.java
+++ src/main/java/com/fintech/b2b/B2bApplication.java
@@ -2,8 +2,10 @@ package com.fintech.b2b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class B2bApplication {
 
 	public static void main(String[] args) {
